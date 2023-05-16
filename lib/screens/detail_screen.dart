ea76388381c216765cb40ca11eb4b9a53cd068dd
leeyejin1231 <82494506+leeyejin1231@users.noreply.github.com>
2023-05-17 04:13:51 +0900

    [feat] make TabBar

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:jolup/screens/meeting_screen.dart';
+import 'package:jolup/screens/qna_screen.dart';
 
-class DetailScreen extends StatefulWidget {
+class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
 
 //   final String title, date;
@@ -12,37 +14,54 @@ class DetailScreen extends StatefulWidget {
 //   });
 
   @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
-
-class _DetailScreenState extends State<DetailScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF9F9F9),
-      appBar: AppBar(
-        title: Column(
-          children: [
-            const Text(
-              '클라이언트 1차 미팅 -1',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.w600,
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        backgroundColor: const Color(0xFFF9F9F9),
+        appBar: AppBar(
+          title: Column(
+            children: [
+              const Text(
+                '클라이언트 1차 미팅 -1',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-            Text(
-              '2018년 2월 3일 토요일',
-              style: TextStyle(
-                fontSize: 18,
-                color: Theme.of(context).textTheme.displayLarge!.color,
+              Text(
+                '2018년 2월 3일 토요일',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Theme.of(context).textTheme.displayLarge!.color,
+                ),
               ),
-            ),
+            ],
+          ),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          toolbarHeight: 100,
+          bottom: const TabBar(
+            labelColor: Colors.black,
+            labelStyle: TextStyle(fontSize: 18),
+            tabs: <Widget>[
+              Tab(
+                text: '회의록',
+              ),
+              Tab(
+                text: '질문하기',
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            MeetingScreen(),
+            QnAScreen(),
           ],
         ),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
-        elevation: 0,
-        toolbarHeight: 100,
       ),
     );
   }
